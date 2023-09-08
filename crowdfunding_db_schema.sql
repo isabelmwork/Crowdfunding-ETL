42e26e02b20077c41e456c6bad79a788f8b1e6f6
isabelmwork <isabelmwork@gmail.com>
2023-09-07 18:27:28 -0700

    added SELECT for display

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -43,3 +43,8 @@ CREATE TABLE campaign(
 	FOREIGN KEY(category_id) REFERENCES category (category_id),
 	FOREIGN KEY(subcategory_id) REFERENCES subcategory (subcategory_id)
 );
+
+SELECT * FROM campaign;
+SELECT * FROM contacts;
+SELECT * FROM category;
+SELECT * FROM subcategory;
